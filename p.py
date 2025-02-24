32f7aa88888a753a9dfe637b9778192328b063f6
kmk021 <kusumamani021@gmail.com>
2025-02-24 15:55:47 +0530

    New commit

--- p.py
+++ p.py
@@ -1,7 +1,10 @@
-p=input("Are you hugry:")
+p=input("Are you hungry:")
 if p=="Yes":
     print("Eat Samosa")
     print("Eat Pizza")
+    print("Eat Burger")
 else:
-    print("do your homework?")
+    thirsty=input("Are you thirsty")
+    if thirsty=="Yes":
+        print("Drink Water")
     
